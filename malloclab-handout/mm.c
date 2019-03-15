43cb7143e574c741d38fff882b09b5fc6c1dd2f0
AaronE <aaronempedrado@gmail.com>
2019-03-15 03:46:13 -0500

    invariant func type casting

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -304,8 +304,11 @@ static void *extend_heap(size_t words)
 /* Check invariant of the prologue and epilogue blocks */
 static int check_invariant(void){
      // Check the prologue block is 8 byte allocated 
-    static char * PRO_BLKP = heap_listp, PRO_HDRP = HDRP(PRO_BLKP), PRO_FTRP = FTRP(PRO_BLKP),
-           EPI_BLPK = mem_heap_hi();
+    char *PRO_BLKP = heap_listp;
+    char *PRO_HDRP = HDRP(PRO_BLKP);
+    char *PRO_FTRP = FTRP(PRO_BLKP);
+    char *EPI_BLPK = mem_heap_hi();
+    
     //Check the prologue block's invariance
     if(!(GET_SIZE(PRO_HDRP) == 8 && GET_ALLOC(PRO_HDRP) == 1 &&
          GET_SIZE(PRO_FTRP) == 8 && GET_ALLOC(PRO_FTRP) == 1)) {
