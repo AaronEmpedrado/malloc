70a7717647c279d43e26449a97a3e3573b792c4a
AaronE <aaronempedrado@gmail.com>
2019-03-14 23:39:00 -0500

    implemented place function

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -51,6 +51,7 @@ team_t team = {
 static void *extend_heap(size_t words);
 static void *coalesce(void *bp);
 static void *find_fit(size_t asize);
+static void place(void *bp, size_t asize);
 
 
 /* Basic constants and macros that I added */
@@ -242,7 +243,7 @@ void *mm_realloc(void *ptr, size_t size)
     return newptr;
 }
 
-
+/* Finds where the block we want to allocate should go */
 static void *find_fit(size_t asize) {
     /* First fit search */
     void *bp;
@@ -255,6 +256,20 @@ static void *find_fit(size_t asize) {
     return NULL;    /* No fit */
 }
 
-
-
+/* Function to actually place the allocated block */
+static void place(void *bp, size_t asize) {
+    size_t csize = GET_SIZE(HDRP(bp));
+    
+    if ((csize - asize) >= (2*DSIZE)) {
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
+        bp = NEXT_BLKP(bp);
+        PUT(HDRP(bp), PACK(csize-asize, 0));
+        PUT(FTRP(bp), PACK(csize-asize, 0));
+    }
+    else {
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
+    }
+}
 
